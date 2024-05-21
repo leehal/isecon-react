4af54a6214f96dc163e73876fdc6d6a2dc511c27
hojunahn <hojunahn@naver.com>
2024-05-21 17:31:29 +0900

    했어요

--- src/pages/main/header.jsx
+++ src/pages/main/header.jsx
@@ -1,23 +1,48 @@
 import styled from "styled-components";
 
 const HeadBox = () => {
+  const Container = styled.div`
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    background: blue;
+    overflow: hidden;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  `;
+  const MainImg = styled.div``;
   const Header = styled.div`
+    position: absolute;
+    top: 0;
     width: 100vw;
     height: 100px;
-    background: red;
+    border: 1px solid red;
   `;
-  const LeftBox = styled.div``;
+  const LeftBox = styled.div`
+    img {
+      width: 150px;
+      height: auto;
+    }
+  `;
   const Logo = styled.div``;
   const RightBox = styled.div``;
   return (
-    <Header>
-      <LeftBox>
-        <Logo>
-          <img src="" alt="" />
-        </Logo>
-      </LeftBox>
-      <RightBox></RightBox>
-    </Header>
+    <Container>
+      <Header>
+        <LeftBox>
+          <Logo>
+            <img src="img/logo.webp" alt="" />
+          </Logo>
+        </LeftBox>
+        <RightBox></RightBox>
+      </Header>
+      <MainImg>
+        <img src="img/main1.png" alt="" />
+      </MainImg>
+    </Container>
   );
 };
 
